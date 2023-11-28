24aca4fafa8e4ece40527acb309270d1c7d0cdf1
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2023-11-28 10:52:23 -0300

    Exericios de imprimir uma msg <Olá, Mundo>

---
+++ Exercicios/ex001/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Exercicio 01</h1>
+    <?php 
+        $msg = "Olá, Mundo"; //Variável de string
+        echo "<p> {$msg} </p>" //Imprime o valor da variável $msg dentro de um tag html
+    ?>
+</body>
+</html>
