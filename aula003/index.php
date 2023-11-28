68153d8f746eead1b9240e5a81d3d4d0cd8c5072
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2023-11-28 09:55:11 -0300

    aula de super tags php no html

---
+++ aula003/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+       
+    </title>
+</head>
+<body>
+    <?php //Super tag PHP, no qual pode ser inserido vários 
+        $nome = "joão";
+    ?>
+    <?= "<p>O meu nome é $nome</p>"; ?><!--Short tag php qie tem o intuido de inserir somente uma comando, um echo ou print-->
+</body>
+</html>
