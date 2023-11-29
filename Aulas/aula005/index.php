531e9ffa1fafcd5951dba880324889c8959b62e6
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2023-11-29 09:57:04 -0300

    Aula de variavéis

---
+++ Aulas/aula005/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        //declaração de variável de snake_case
+        $curso_superior = "Engenharia";
+        //Declaração de variável CameICase
+        $nomeCompleto = 'João Vitor Amaro de Melo'; 
+        $nome = "João"; //Declaração de variável normal
+        $salário = 1.320; //Declaração de variável com acento
+        $idadeø = 15; //Declaração de variável com símbolos da tabela ASCII
+        const _Tx = 0; // Declaração de constantes
+        echo "<p>Muito prazer, $nome!</p>";
+        echo "<p>O seu salário é $salário</p>";
+        echo "<p>A sua idade é $idadeø</p>"
+    ?>
+</body>
+</html>
