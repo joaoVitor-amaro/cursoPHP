52460e5fab1420f79d7e8e4dfb648af0c941f60c
João Vitor Amaro de Melo <106560064+joaoVitor-amaro@users.noreply.github.com>
2023-12-02 20:47:40 -0300

    Aula de sobre String PHP

---
+++ Aulas/aula007/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        const estado = "PB";
+        $number = 5;
+        $nome = "João";
+        $apelido = "Vitin";
+        $sobre_nome = "Vitor";
+        //Aspas dupla interpreta o conteúdo da string e exibe o valor da variável
+        echo "<p>O número é ".$number." \u{1F418}</p>";
+        //Aspas simples não interprata a string e não exibe o valor da variável
+        echo '<p>O número é ".$number." \u{1F418}</p>';
+        //Aspas dupla não faz a interpolação interna
+        echo "<p>Moro no estado</p>";
+        //As constante só pode ser concatenada com o operador de concatenação(.)
+        echo "<p>Moro na ".estado."</p>";
+        //erro semântico
+        echo '<p>$nome "Minotauro" $sobre_nome';
+
+
+        //Tipos de sequência de escape
+
+        //Escape de aspas dupla
+        echo "<p>$nome \"$apelido\" $sobre_nome</p>";
+        //Escape de tabulação, porém, não funciona no html
+        echo "<p>$nome \t\t \"$apelido\" $sobre_nome</p>"
+    ?>
+</body>
+</html>
